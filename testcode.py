a5f8fd232f974f144ccb1db8ab25a0d386dca80a
jr <EnocMenaGit@gmail.com>
2022-07-06 20:38:59 -0400

    *Added menubar

--- testcode.py
+++ testcode.py
@@ -1,7 +1,5 @@
 # use the code below for the "search by artist" menu item
-
-from youtubesearchpython import VideosSearch
-from pprint import pprint
-
+"""
 videosSearch = VideosSearch('wonderwall', limit=5)
 pprint(videosSearch.result())
+"""
